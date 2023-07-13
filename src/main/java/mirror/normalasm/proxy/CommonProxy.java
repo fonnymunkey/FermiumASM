LoliKingdom/LoliASM

Add option to adjust thread priorities (#162)
Client thread given default priority + 2
Server thread given min priority + 2
Chunk builder threads given min priority + 1

Number of chunk builders is also adjusted to match Sodium
defaults on modern versions

src/main/java/zone/rong/loliasm/proxy/CommonProxy.java
@@ -56,6 +56,8 @@ public class CommonProxy {
         if (LoliConfig.instance.cleanupLaunchClassLoaderEarly) {
             cleanupLaunchClassLoader();
         }
+        if (LoliConfig.instance.threadPriorityFix)
+            Thread.currentThread().setPriority(Thread.NORM_PRIORITY + 2);
     }
 
     public void preInit(FMLPreInitializationEvent event) { }

fonnymunkey/FermiumASM

(cherry picked from commit f73ce5bdc3178f7bf0cb9d9b62d0315a09cc63ad)

src/main/java/mirror/normalasm/proxy/CommonProxy.java
@@ -56,6 +56,8 @@ public class CommonProxy {
         if (NormalConfig.instance.cleanupLaunchClassLoaderEarly) {
             cleanupLaunchClassLoader();
         }
+        if (NormalConfig.instance.threadPriorityFix)
+            Thread.currentThread().setPriority(Thread.NORM_PRIORITY + 2);
     }
 
     public void preInit(FMLPreInitializationEvent event) { }