940ec99cc223db6fb0e0ff6aabd6089ccc888054
fonnymunkey <fonnymunkey@gmail.com>
2024-02-14 23:52:20 -0500

    FermiumBooter dependency, bump version 5.20
    Add FermiumBooter as hard dependency to stop issue reports, atleast for now

--- src/main/java/mirror/normalasm/core/NormalLoadingPlugin.java
+++ src/main/java/mirror/normalasm/core/NormalLoadingPlugin.java
@@ -28,7 +28,7 @@ import java.util.Map;
 @IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
 public class NormalLoadingPlugin implements IFMLLoadingPlugin {
 
-    public static final String VERSION = "5.19";
+    public static final String VERSION = "5.20";
 
     public static final boolean isDeobf = FMLLaunchHandler.isDeobfuscatedEnvironment();
 
