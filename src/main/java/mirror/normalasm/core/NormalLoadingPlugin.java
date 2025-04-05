0fade99cfd0d5d946f3ffec8e91000a32844c5b9
fonnymunkey <fonnymunkey@gmail.com>
2025-04-05 18:52:16 -0400

    5.28
    Bump version

--- src/main/java/mirror/normalasm/core/NormalLoadingPlugin.java
+++ src/main/java/mirror/normalasm/core/NormalLoadingPlugin.java
@@ -27,7 +27,7 @@ import java.util.Map;
 @IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
 public class NormalLoadingPlugin implements IFMLLoadingPlugin {
 
-    public static final String VERSION = "5.25";
+    public static final String VERSION = "5.28";
 
     public static final boolean isDeobf = FMLLaunchHandler.isDeobfuscatedEnvironment();
 
